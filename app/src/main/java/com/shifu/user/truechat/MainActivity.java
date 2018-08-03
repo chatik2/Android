de59a7218a83085bc0f3bb54b2a2584921e15f53
yarplux <yarplux@yandex.ru>
2018-08-03 10:01:29 +0300

    first commit

---
+++ app/src/main/java/com/shifu/user/truechat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shifu.user.truechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
